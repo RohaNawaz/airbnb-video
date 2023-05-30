ae1b244de758bda57d3df96922f0da26f92409aa
RohaNawaz <101508514+RohaNawaz@users.noreply.github.com>
2023-05-30 22:14:10 +0500

    login function

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -7,6 +7,7 @@ import MenuItem from './MenuItem';
 import useRegisterModal from '@/app/hooks/useLoginModal';
 import useLoginModal from '@/app/hooks/useLoginModal';
 import { User } from '@prisma/client';
+import { signOut } from 'next-auth/react';
 
 interface UserMenuProps {
     currentUser?: User | null
@@ -62,7 +63,7 @@ const UserMenu: React.FC<UserMenuProps> = ({
                       label="Airbnb my home" />
                       <hr />
                       <MenuItem 
-                      onClick={() => {}} 
+                      onClick={() => signOut()} 
                       label="Logout" />
                      </>       
                     ) : (
