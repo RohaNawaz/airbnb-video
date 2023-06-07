fe97ff104afba329079237bef5890d4d6654b551
RohaNawaz <101508514+RohaNawaz@users.noreply.github.com>
2023-06-07 21:57:46 +0500

    listing creation step 4

--- app/components/Map.tsx
+++ app/components/Map.tsx
@@ -30,8 +30,16 @@ const Map: React.FC<MapProps> = ({
         scrollWheelZoom={false}
         className="h-[35hv] rounded-lg" 
        >
-
-        </MapContainer>
+        <TileLayer
+       attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+       url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+       />
+       {center && (
+          <Marker 
+          position={center as L.LatLngExpression}
+          />
+       )}
+      </MapContainer> 
     )
 };
 
