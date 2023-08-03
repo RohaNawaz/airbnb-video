ab1f97d3615c7cfa0be4ca2784ab93d032d05efe
RohaNawaz <101508514+RohaNawaz@users.noreply.github.com>
2023-08-03 17:58:37 +0500

    chasnges

--- app/api/reservations/route.ts
+++ app/api/reservations/route.ts
@@ -1,44 +1,44 @@
-import { NextResponse } from "next/server";
+// import { NextResponse } from "next/server";
 
-import prisma from "@/app/libs/prismadb";
-import getCurrentUser from "@/app/actions/getCurrentUser";
+// import prisma from "@/app/libs/prismadb";
+// import getCurrentUser from "@/app/actions/getCurrentUser";
 
-export async function POST(
-  request: Request, 
-) {
-  const currentUser = await getCurrentUser();
+// export async function POST(
+//   request: Request, 
+// ) {
+//   const currentUser = await getCurrentUser();
 
-  if (!currentUser) {
-    return NextResponse.error();
-  }
+//   if (!currentUser) {
+//     return NextResponse.error();
+//   }
 
-  const body = await request.json();
-  const { 
-    listingId,
-    starDate,
-    endDate,
-    totalPrice
-   } = body;
+//   const body = await request.json();
+//   const { 
+//     listingId,
+//     starDate,
+//     endDate,
+//     totalPrice
+//    } = body;
 
-   if (!listingId || !starDate || !endDate || !totalPrice) {
-    return NextResponse.error();
-  }
+//    if (!listingId || !starDate || !endDate || !totalPrice) {
+//     return NextResponse.error();
+//   }
 
-  const listingAndReservation = await prisma.listing.update({
-    where: {
-      id: listingId
-    },
-    data: {
-      reservations: {
-        create: {
-          userId: currentUser.id,
-          starDate,
-          endDate,
-          totalPrice,
-        }
-      }
-    }
-  });
+//   const listingAndReservation = await prisma.listing.update({
+//     where: {
+//       id: listingId
+//     },
+//     data: {
+//       reservations: {
+//         create: {
+//           userId: currentUser.id,
+//           starDate,
+//           endDate,
+//           totalPrice,
+//         }
+//       }
+//     }
+//   });
 
-  return NextResponse.json(listingAndReservation);
-}
+//   return NextResponse.json(listingAndReservation);
+// }
